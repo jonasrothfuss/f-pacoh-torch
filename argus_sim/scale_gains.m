63cf3167a930c50126599cd97c918ef4d2611761
Jonas Rothfuss <jonas.rothfuss@gmx.de>
2021-08-10 17:12:43 +0200

    updated version of argus sim module

--- argus_sim/scale_gains.m
+++
@@ -1,9 +0,0 @@
-function [Kp,Kv,Ki,Ka_ff] = scale_gains(SLPKP,SLVKP,SLVKI,SLAFF,scal_fac,SLVRAT)
-%Rescales the gains to their unit in the Argus Simulink model
-Kp = SLPKP;
-Kv = SLVKP*scal_fac*SLVRAT/(1024);
-Ki = SLVKI/2^16;
-Ka_ff = SLAFF/2e7;
-
-end
-
